4628b05a5a33ffcb618997cf7e75d077e8e44b03
Keystone Tech <keystone-tech@example.com>
2026-05-22 18:56:48 +0300

    fix(db): make all recent migrations idempotent to fix deployment

--- bot/migrations/alembic/versions/c4a72e00fcdf_create_push_subscriptions.py
+++ bot/migrations/alembic/versions/c4a72e00fcdf_create_push_subscriptions.py
@@ -19,21 +19,24 @@ depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
-    op.create_table(
-        'push_subscriptions',
-        sa.Column('id', sa.Integer(), nullable=False),
-        sa.Column('user_id', sa.Integer(), nullable=False),
-        sa.Column('endpoint', sa.Text(), nullable=False),
-        sa.Column('p256dh', sa.String(length=255), nullable=False),
-        sa.Column('auth', sa.String(length=255), nullable=False),
-        sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
-        sa.Column('updated_at', sa.DateTime(timezone=True), nullable=True),
-        sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
-        sa.PrimaryKeyConstraint('id'),
-        sa.UniqueConstraint('endpoint')
-    )
-    op.create_index(op.f('ix_push_subscriptions_id'), 'push_subscriptions', ['id'], unique=False)
-    op.create_index(op.f('ix_push_subscriptions_user_id'), 'push_subscriptions', ['user_id'], unique=False)
+    conn = op.get_bind()
+    insp = sa.inspect(conn)
+    if 'push_subscriptions' not in insp.get_table_names():
+        op.create_table(
+            'push_subscriptions',
+            sa.Column('id', sa.Integer(), nullable=False),
+            sa.Column('user_id', sa.Integer(), nullable=False),
+            sa.Column('endpoint', sa.Text(), nullable=False),
+            sa.Column('p256dh', sa.String(length=255), nullable=False),
+            sa.Column('auth', sa.String(length=255), nullable=False),
+            sa.Column('created_at', sa.DateTime(timezone=True), nullable=True),
+            sa.Column('updated_at', sa.DateTime(timezone=True), nullable=True),
+            sa.ForeignKeyConstraint(['user_id'], ['users.id'], ondelete='CASCADE'),
+            sa.PrimaryKeyConstraint('id'),
+            sa.UniqueConstraint('endpoint')
+        )
+        op.create_index(op.f('ix_push_subscriptions_id'), 'push_subscriptions', ['id'], unique=False)
+        op.create_index(op.f('ix_push_subscriptions_user_id'), 'push_subscriptions', ['user_id'], unique=False)
 
 
 def downgrade() -> None:
